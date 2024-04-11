25d4a5f9640e8129210ec044753059eb2c5a3a76
roberto.mazzalai <mazzalairob@gmail.com>
2024-04-11 22:33:16 +0200

    fix telescope

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,22 +1,21 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 local plugins = {
     "tanvirtin/monokai.nvim",
-
+    'nvim-lua/plenary.nvim', 
     {
         'nvim-telescope/telescope.nvim', tag = '0.1.6',
-        dependecies = { 'nvim-lua/plenary.nvim' }
     },
     { 
         'nvim-treesitter/nvim-treesitter', 
