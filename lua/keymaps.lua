beb6c49e6d348b70f9128742580c5bfcda06b0a6
Roberto Mazzalai <roberto.mazzalai@techedgegroup.com>
2024-04-18 15:13:59 +0200

    add neotree open on startup

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -29,6 +29,9 @@ vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
 vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>', opts)
 vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
+-- Select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
 -----------------
 -- Visual mode --
 -----------------
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -12,5 +12,10 @@ return {
 
 		vim.g.loaded_netrw = 1
 		vim.g.loaded_netrwPlugin = 1
+
+--		require("neo-tree").setup({
+--			popup_border_style = "rounded",
+--      enable_git_status = true,
+--		})
 	end,
 }
