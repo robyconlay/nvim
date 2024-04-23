308b538cb07d01063f2e32a9bc99a9ad93475499
Roberto Mazzalai <roberto.mazzalai@techedgegroup.com>
2024-04-23 17:46:20 +0200

    neotree focus open files

--- lua/options.lua
+++ lua/options.lua
@@ -27,3 +27,4 @@ vim.opt.hlsearch = false            -- do not highlight matches
 vim.opt.ignorecase = true           -- ignore case in searches by default
 vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
 
+vim.opt.scrolloff = 8
--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,21 +1,26 @@
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+  "nvim-neo-tree/neo-tree.nvim",
+  branch = "v3.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+    "MunifTanjim/nui.nvim",
+    -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
 
-		vim.g.loaded_netrw = 1
-		vim.g.loaded_netrwPlugin = 1
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-		require("neo-tree").setup({
-			--			popup_border_style = "rounded",
-			--      enable_git_status = true,
-		})
-	end,
+    require("neo-tree").setup({
+      --			popup_border_style = "rounded",
+      --      enable_git_status = true,
+      filesystem = {
+        follow_current_file = {
+          enabled = true,
+        },
+      },
+    })
+  end,
 }
